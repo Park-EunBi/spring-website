93bb4736c4a0a4d3c39acc622ac24f90ad074383
Park-EunBi <parkeb555@gmail.com>
2022-07-12 16:41:25 +0900

    FEAT: 게시물 등록 화면, 기능 생성

---
+++ demo/src/main/resources/static/js/app/index.js
@@ -0,0 +1,41 @@
+// index.js 만의 유효 범위를 만들어서
+// index 객체 안에서만 function 이 유효하도록 설정
+// 중복된 함수 이름 문제 방지
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
